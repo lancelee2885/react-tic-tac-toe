57d89f7897f01c5eac51116f52577cc34bd1f27c
Lance L <lance@Lances-MacBook-Air.local>
2021-10-15 02:02:32 -0700

    display winning message

--- src/Game.js
+++ src/Game.js
@@ -17,26 +17,37 @@ const Game = function () {
   const [isWon, setIsWon] = useState(false);
   const [turn, setTurn] = useState('p1');
 
-
   function mark(turn, i, j) {
     let nextPlayer = turn === 'p1' ? 'p2' : 'p1';
-    if(gameBoard[i][j]) {
+    if (gameBoard[i][j]) {
       return null;
     };
     let clone = [...gameBoard];
     clone[i][j] = turn;
     setGameBoard(clone);
     setTurn(nextPlayer);
+    setIsWon(checkWinH(gameBoard, i, j));
+    // checkWinV(gameBoard, i, j);
+    // checkWinD(gameBoard, i, j);
+  }
+
+  function checkWinH(gameBoard, i) {
+
+    if (gameBoard[i][0] === gameBoard[i][1] &&
+      gameBoard[i][1] === gameBoard[i][2]) {
+      return true;
+    }
   }
 
   return (
-    <div>
+    <div className='game'>
+      {isWon ? <p>{turn} won</p>: <p>placeholder</p>}
       <table className='gameBoard'>
         <tbody>
           {gameBoard.map((r, i, a) =>
             <tr key={`row${i + 1}`}>
               {a[i].map((b, j) =>
-                <Box gameBoard={gameBoard} row={i} col={j} turn={turn} mark={mark}/>)}
+                <Box gameBoard={gameBoard} row={i} col={j} turn={turn} mark={mark} />)}
             </tr>)}
         </tbody>
       </table>
